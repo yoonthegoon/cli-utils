c55c42744ba530285976017a6dce7f1584037dd3
Yunis Yilmaz <yunis@Yuniss-MacBook-Pro.local>
2024-11-30 02:29:26 -0500

    remove serde, toml

--- src/error.rs
+++ src/error.rs
@@ -3,17 +3,10 @@ pub enum Error {
     ConfigDoesNotExist,
     Io(std::io::Error),
     OptionNone,
-    TomlSer(toml::ser::Error),
 }
 
 impl From<std::io::Error> for Error {
     fn from(value: std::io::Error) -> Self {
         Error::Io(value)
-    }
-}
-
-impl From<toml::ser::Error> for Error {
-    fn from(value: toml::ser::Error) -> Self {
-        Error::TomlSer(value)
     }
 }
